56997880b00e243f65b94b506f70f90bde657d61
Rabby <mdrabbybd4747@gmail.com>
2024-11-14 14:25:24 +0600

    load all image with gsap animation and install locomotive-scroll for smoth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { useEffect } from "react"
 import Canvas from "./Canvas"
+import data from "./data"
+import LocomotiveScroll from "locomotive-scroll"
 
 function App() {
+	useEffect(() => {
+		const locomotiveScroll = new LocomotiveScroll()
+	}, [])
+
 	return (
 		<>
-			<div className="w-full min-h-screen bg-black text-white">
-				<Canvas />
+			<div className="w-full relative min-h-screen ">
+				{data[0].map((canvasDetails, index) => (
+					<Canvas key={index} details={canvasDetails} />
+				))}
+				<div className="w-full h-screen text-white">
+					<nav className="fixed top-0 left-0 w-full p-8 flex justify-between z-50">
+						<div className="brand text-2xl font-semibold">ThirtySixStudios</div>
+						<div className="links flex gap-10">
+							{["Home", "About", "Projects", "Contact"].map((link, index) => (
+								<a key={index} href={`#${link.toLocaleLowerCase()}`}>
+									{link}
+								</a>
+							))}
+						</div>
+					</nav>
+				</div>
 			</div>
 		</>
 	)
--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,26 +1,57 @@
-import { useEffect, useRef } from "react"
+/* eslint-disable react/prop-types */
+import { useEffect, useRef, useState } from "react"
 import canvasImage from "./canvasImage"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
 
-const Canvas = () => {
+const Canvas = ({ details }) => {
+	const { startIndex, numImages, duration, size, top, left, zIndex } = details
+	const [index, setIndex] = useState({ value: startIndex })
 	const canvasRef = useRef(null)
 
+	useGSAP(() => {
+		gsap.to(index, {
+			value: startIndex + numImages - 1,
+			duration: duration,
+			repeat: -1,
+			ease: "linear",
+			onUpdate: () => {
+				setIndex({ value: Math.round(index.value) })
+			},
+		})
+	})
+
 	useEffect(() => {
+		const scale = window.devicePixelRatio
 		const canvas = canvasRef.current
 		const context = canvas.getContext("2d")
 		const image = new Image()
-		image.src = canvasImage[80]
+		image.src = canvasImage[index.value]
 		image.onload = () => {
-			canvas.width = image.width
-			canvas.height = image.height
-			context.drawImage(image, 0, 0)
+			canvas.width = canvas.offsetWidth * scale
+			canvas.height = canvas.offsetHeight * scale
+			canvas.style.width = canvas.offsetWidth + "px"
+			canvas.style.width = canvas.offsetHeight + "px"
+			context.scale(scale, scale)
+
+			context.drawImage(image, 0, 0, canvas.offsetWidth, canvas.offsetHeight)
 		}
-	}, [])
+	}, [index])
 
 	return (
 		<canvas
+			data-scroll
+			data-scroll-speed={Math.random().toFixed(1)}
 			ref={canvasRef}
 			id="canvas"
-			className="w-[18rem] h-[18rem]"
+			className="absolute"
+			style={{
+				width: `${size * 1.3}px`,
+				height: `${size * 1.3}px`,
+				top: `${top}%`,
+				left: `${left}%`,
+				zIndex: `${zIndex}`,
+			}}
 		></canvas>
 	)
 }
